c20795dbf62d41f8d3a942389c86671b50d59810
Philipp Huynh <bgbphilipp@gmail.com>
2026-01-16 12:35:28 +0100

    fix: update base path in vite.config.ts to root

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/personal_website/",
+  base: "/",
   resolve: {
     alias: {
       '@': '/src',
