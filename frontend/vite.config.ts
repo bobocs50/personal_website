ccef3c4add6e389de77cc4a6f48f8c35d5267043
Philipp Huynh <bgbphilipp@gmail.com>
2026-01-16 12:26:32 +0100

    feat: add @types/node and undici-types, update vite.config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/personal_website"
+  base: process.env.VITE_BASE_PATH || "/personal_website/",
   resolve: {
     alias: {
       '@': '/src',
