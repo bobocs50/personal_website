925bdecf16aec27747e403f40e11be5611f370bb
Philipp Huynh <bgbphilipp@gmail.com>
2026-01-16 12:23:07 +0100

    feat: update favicon and modify title in index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/personal_website"
   resolve: {
     alias: {
       '@': '/src',
