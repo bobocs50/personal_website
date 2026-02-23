f1b637c966a1e8e9af6c31c20f10b885db315218
Philipp Huynh <bgbphilipp@gmail.com>
2026-02-23 23:25:34 +0100

    removed old website

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import Navigation from "@/components/navigation"
-import WorksHero from "@/components/works-hero"
-import FeaturedWorks from "@/components/featured-works"
-import FooterSection from "@/components/footer-section"
-
 export default function WorksPage() {
   return (
-    <main className="min-h-screen bg-background">
-      <Navigation />
-      <WorksHero />
-      <FeaturedWorks />
-      <FooterSection />
-    </main>
+    <main className="min-h-screen bg-white" />
   )
 }
